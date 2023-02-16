ffb384c7010d5afb5202c4e65ff3640eaefd9735
wangxuan <3154263578@qq.com>
2023-02-16 20:13:38 +0800

    git第二次提交到本地仓库MyProject01

--- git-text-project/src/com/itheima/git/My.java
+++ git-text-project/src/com/itheima/git/My.java
@@ -7,7 +7,8 @@ package com.itheima.git;
 public class My {
     public static void main(String args[]) {
         System.out.println("你好");
-        System.out.println("你好啊login分支");
 
+        System.out.println("你好啊login分支");
+        System.out.println("你好啊login分支");
     }
 }
