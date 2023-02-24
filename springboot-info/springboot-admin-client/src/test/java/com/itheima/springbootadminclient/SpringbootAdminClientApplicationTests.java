da4cd24e1e520e7b20536097d6dabe67a4181dbf
wangxuan <3154263578@qq.com>
2023-02-24 12:49:00 +0800

    springboot-admin图形化界面使用-server服务器端，有个@EnableAdminServer注解问题

---
+++ springboot-info/springboot-admin-client/src/test/java/com/itheima/springbootadminclient/SpringbootAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootadminclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
