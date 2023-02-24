fcf40bd89b019412c0bbfbd170410ee842875232
wangxuan <3154263578@qq.com>
2023-02-24 13:58:01 +0800

    war包方式演示

--- springboot-info/src/main/java/com/itheima/SpringbootInfoApplication.java
+++ springboot-info/src/main/java/com/itheima/SpringbootInfoApplication.java
@@ -12,3 +12,33 @@ public class SpringbootInfoApplication {
     }
 
 }
+
+
+
+
+
+/*
+打成war包的方式演示
+首先将pom.xml加上
+<package>war</package>
+<！--给war包起别名-->
+<build>
+<finalName>springboot</finalName>
+</build>
+
+
+@SpringBootApplication
+public class SpringbootInfoApplication extends SpringBootServerInitializer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootInfoApplication.class, args);
+    }
+
+    @override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+    return builder.sources(SpringbootDeployApplication.class);
+
+    }
+
+}*/
