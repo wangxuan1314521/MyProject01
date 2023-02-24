a973370f252b5710cd82d8d7ac6baaa90532c51c
wangxuan <3154263578@qq.com>
2023-02-24 12:54:28 +0800

    springboot admin图形化界面服务器端，有一个注解没有成功。 @EnableAdminServer

---
+++ springboot-info/springboot-admin-server/src/main/java/com/itheima/springbootadminserver/SpringbootAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableAdminServer
+@SpringBootApplication
+public class SpringbootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminServerApplication.class, args);
+    }
+
+}
---
+++ springboot-info/springboot-admin-server/src/test/java/com/itheima/springbootadminserver/SpringbootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootadminserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
